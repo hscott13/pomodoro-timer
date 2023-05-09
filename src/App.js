0b835472ddbdb98b3474dd716ff1fff5d0e54afe
hscott13 <hscott13@hotmail.co.uk>
2023-05-09 14:38:32 +0100

    File set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pomodoro Timer</h1>
+      <div>00:00</div>
+      <button>Begin</button>
+    </div>
+  );
+}
+
+export default App;
