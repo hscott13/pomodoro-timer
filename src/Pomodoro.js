3a9d06c32da4f86dea139692f37c3deed3ab1398
hscott13 <hscott13@hotmail.co.uk>
2023-05-16 11:36:49 +0100

    Added timer logic

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Pomodoro() {
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+  const [displayMessage, setDisplayMessage] = useState(false);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      clearInterval(interval);
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          let minutes = displayMessage ? 24 : 4;
+          let seconds = 59;
+
+          setSeconds(seconds);
+          setMinutes(minutes);
+          setDisplayMessage(!displayMessage);
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [seconds, minutes, displayMessage]);
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
   return (
     <div className="App">
       <h1>Pomodoro Timer</h1>
-      <div class="time">00:00</div>
+      <div className="time">
+        {timerMinutes}:{timerSeconds}
+      </div>
       <button>Begin</button>
       <button>Pause</button>
+      <div className="message">{displayMessage && <div>Break Time!</div>}</div>
     </div>
   );
 }
