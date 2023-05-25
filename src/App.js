24774bc112620695a3c5490639796a73c58d7b19
hscott13 <hscott13@hotmail.co.uk>
2023-05-25 14:32:04 +0100

    Added night and day mode buttons

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import "./App.css";
 import Pomodoro from "./Pomodoro";
 
 function App() {
-  return <Pomodoro />;
+  return (
+    <div className="container">
+      <div className="modes">
+        <button>
+          <i class="fa-solid fa-sun on"></i>
+        </button>
+        <button>
+          <i class="fa-solid fa-moon off"></i>
+        </button>
+      </div>
+      <Pomodoro />
+    </div>
+  );
 }
 
 export default App;
